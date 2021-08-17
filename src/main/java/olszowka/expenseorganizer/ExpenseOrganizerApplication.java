3063a2218f2f50728b4a6466f4960ab3a79f6974
ToJoos <joosx2000@gmail.com>
2021-08-17 20:27:31 +0200

    Initial commit.

---
+++ src/main/java/olszowka/expenseorganizer/ExpenseOrganizerApplication.java
@@ -0,0 +1,12 @@
+package olszowka.expenseorganizer;
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseOrganizerApplication {
+
+    public static void main(String[] args) {
+        Application.launch(JavaFxApplication.class, args);
+    }
+}
---
+++ src/test/java/olszowka/expenseorganizer/ExpenseOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package olszowka.expenseorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
