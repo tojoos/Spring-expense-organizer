3b70046f84990ebf7c26c005fdb0be7d9df690ba
ToJoos <joosx2000@gmail.com>
2021-08-18 09:37:06 +0200

    Icon file changed

--- src/main/java/olszowka/expenseorganizer/JavaFxApplication.java
+++ src/main/java/olszowka/expenseorganizer/JavaFxApplication.java
@@ -34,7 +34,7 @@ public class JavaFxApplication extends Application {
         FxWeaver fxWeaver = applicationContext.getBean(FxWeaver.class);
         Parent root = fxWeaver.loadView(MainStageController.class);
         Scene scene = new Scene(root);
-        stage.getIcons().add(new Image(String.valueOf(getClass().getClassLoader().getResource("icon\\expense_icon.png"))));
+        stage.getIcons().add(new Image(String.valueOf(getClass().getClassLoader().getResource("icon\\icon.png"))));
         stage.setTitle("Expense Organizer");
         stage.setScene(scene);
         stage.show();
