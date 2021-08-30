5c811f5410d3c2e8a29b56ab7420ccc1e5e1431b
ToJoos <joosx2000@gmail.com>
2021-08-30 13:04:33 +0200

    Delete bootstrap data (not needed due to loading from .JSON files.

--- src/main/java/olszowka/expenseorganizer/bootstrap/DataLoader.java
+++ src/main/java/olszowka/expenseorganizer/bootstrap/DataLoader.java
@@ -25,22 +25,6 @@ public class DataLoader implements CommandLineRunner {
     }
 
     private void loadData() {
-        Income income1 = new Income("Scholarship 08/22", "600.00", "Scholarship");
-        incomeService.addPosition(income1);
 
-        Income income2 = new Income("Salary 08/21", "120.00", "Primary Job", LocalDate.of(2021,8,4));
-        incomeService.addPosition(income2);
-
-        Income income3 = new Income("Salary 07/21", "120.00", "Primary Job", LocalDate.of(2021,7,1));
-        incomeService.addPosition(income3);
-
-        Outcome outcome1 = new Outcome("Pizza", "23.00", "Food");
-        outcomeService.addPosition(outcome1);
-
-        Outcome outcome2 = new Outcome("Kebab", "12.50", "Food", LocalDate.of(2021,8,4));
-        outcomeService.addPosition(outcome2);
-
-        Outcome outcome3 = new Outcome("Mieso", "22.50", "Food", LocalDate.of(2021,7,10));
-        outcomeService.addPosition(outcome3);
     }
 }
