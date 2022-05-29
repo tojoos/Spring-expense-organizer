8a3b70fb3fe0003cbbf728b91d61d1ce737f360a
ToJoos <joosx2000@gmail.com>
2022-05-29 15:34:28 +0200

    Added calculation service #11. Binded it with combobox.

---
+++ src/main/java/olszowka/expenseorganizer/services/CalculationService.java
@@ -0,0 +1,40 @@
+package olszowka.expenseorganizer.services;
+
+import olszowka.expenseorganizer.model.Position;
+import olszowka.expenseorganizer.model.Timeframe;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CalculationService {
+
+    public <T extends Position> List<T> getPeriodicPositions(List<T> positionList, Timeframe timeframe) {
+        List<T> positionsPeriodically = new ArrayList<>();
+
+        LocalDate tillDate = getDateForPeriod(timeframe);
+
+        for(T p : positionList) {
+            if(p.getDate().isAfter(tillDate)) {
+                positionsPeriodically.add(p);
+            }
+        }
+
+        return positionsPeriodically;
+    }
+
+    private LocalDate getDateForPeriod(Timeframe timeframe) {
+        if(timeframe == Timeframe.DAY) {
+            return LocalDate.now().minusDays(1);
+        } else if (timeframe == Timeframe.WEEK) {
+            return LocalDate.now().minusWeeks(1);
+        } else if (timeframe == Timeframe.MONTH) {
+            return LocalDate.now().minusMonths(1);
+        } else {
+            return LocalDate.EPOCH;
+        }
+    }
+}
