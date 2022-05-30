99a9583efd66d0250c127e4d5aa4e492beda81fa
ToJoos <joosx2000@gmail.com>
2022-05-30 15:44:25 +0200

    Added indexes to the Timeframe enum type to easily index ComboBox's items.

--- src/main/java/olszowka/expenseorganizer/model/Timeframe.java
+++ src/main/java/olszowka/expenseorganizer/model/Timeframe.java
@@ -1,5 +1,15 @@
 package olszowka.expenseorganizer.model;
 
 public enum Timeframe {
-    DAY, WEEK, MONTH, ALL;
+    DAY(0), WEEK(1), MONTH(2), ALL(3);
+
+    private final int idx;
+
+    private Timeframe(int idx) {
+        this.idx = idx;
+    }
+
+    public int getIdx() {
+        return idx;
+    }
 }
